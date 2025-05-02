cdc100ecbc2177176f564e65777add6461792243
Christian Furquim <christian.furquim@hotmail.com>
2025-05-02 13:21:46 -0300

    Correção no caminho do icone em TotalVendas

--- src/components/TotalVendas.tsx
+++ src/components/TotalVendas.tsx
@@ -3,7 +3,7 @@ const TotalVendas = () => {
       <div className="text-center">
         <div className="flex justify-center items-center mb-4">
           <div className="bg-green-700 text-orange-600 rounded-full w-10 h-10 flex items-center justify-center text-xl">
-            <img src="public/icon/vendas.svg" alt="Total Vendas" className="w-6 h-6" />
+            <img src="/icon/vendas.svg" alt="Total Vendas" className="w-6 h-6" />
           </div>
         </div>
         <p className="text-md text-gray-500 font-medium">Vendas Totais</p>
